dc679345cf8c11425083e7e1a89f719484f0cd01
jmjles <jmjles@gmail.com>
2019-12-06 10:45:29 -0800

    Added Simple Test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,6 +2,12 @@ import React from 'react';
 import {render}from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
+test('Renders without crashing', () => {
   render(<App/>)
 });
+
+test('Data is Retrieved and Rendered', () => {
+  const comp = render(<App/>)
+  comp.findByText('Alex Morgan')
+  comp.findByText('Kelley O\'Hara')
+})
